65a4483db789a7c32759eae1be01f61dc221c146
kasperekd <kasperekd@mail.ru>
2024-02-15 20:54:57 +0700

    added mySimpleComputer.h

---
+++ include/mySimpleComputer.h
@@ -0,0 +1,28 @@
+
+int sc_memoryInit (void);
+int sc_memorySet (int address, int value);
+int sc_memoryGet (int address, int * value);
+int sc_memorySave (char * filename);
+int sc_memoryLoad (char * filename);
+
+int sc_regInit (void);
+int sc_regSet (int register, int value);
+int sc_regGet (int register, int * value);
+
+int sc_accumulatorInit (void);
+int sc_accumulatorSet (int value);
+int sc_accumulatorGet (int * value);
+
+int sc_icounterInit (void);
+int sc_icounterSet (int value);
+int sc_icounterGet (int * value);
+
+int sc_commandEncode (int sign, int command, int operand, int * value);
+int sc_commandDecode (int value, int * sign, int * command, int * operand);
+int sc_commandValidate (int command);
+
+void printCell (int address);
+void printFlags (void);
+void printDecodedCommand (int value);
+void printAccumulator (void);
+void printCounters (void);
