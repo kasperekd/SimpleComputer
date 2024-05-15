6eff9d36dd65ad87c204c7e46229b331fd622947
kasperekd <kasperekd@mail.ru>
2024-05-15 22:41:44 +0700

    sat impl

--- translators/sat.c
+++ translators/sat.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "mySimpleComputer.h"
 
+#define ERR_EXPECTED_ADDRESS_OF_MEMORY_CELL -1
+#define ERR_WRONG_COMMAND -2
+#define ERR_WRONG_OPERAND -3
+#define ERR_BY_ENCODE -4
 
-// Определение кодов операций
 #define READ 0x0A
 #define WRITE 0x0B
 #define LOAD 0x14
@@ -20,88 +23,170 @@
 #define NOT 0x33
 #define AND 0x34
 #define OR 0x35
-#define ASSIGNMENT 0xFF // Код операции для команды "="
 
-// Преобразование символьного обозначения команды в ее код
-int get_opcode(char *command) {
-    if (strcmp(command, "READ") == 0) return READ;
-    if (strcmp(command, "WRITE") == 0) return WRITE;
-    if (strcmp(command, "LOAD") == 0) return LOAD;
-    if (strcmp(command, "STORE") == 0) return STORE;
-    if (strcmp(command, "ADD") == 0) return ADD;
-    if (strcmp(command, "SUB") == 0) return SUB;
-    if (strcmp(command, "DIVIDE") == 0) return DIVIDE;
-    if (strcmp(command, "MUL") == 0) return MUL;
-    if (strcmp(command, "JUMP") == 0) return JUMP;
-    if (strcmp(command, "JNEG") == 0) return JNEG;
-    if (strcmp(command, "JZ") == 0) return JZ;
-    if (strcmp(command, "HALT") == 0) return HALT;
-    if (strcmp(command, "NOT") == 0) return NOT;
-    if (strcmp(command, "AND") == 0) return AND;
-    if (strcmp(command, "OR") == 0) return OR;
-    if (strcmp(command, "=") == 0) return ASSIGNMENT; // Добавлено для команды "="
-    return -1; // Команда не найдена
+FILE *input = NULL;
+
+void load_file(const char* filename)
+{
+	if((input = fopen(filename, "r")) == NULL)
+	{
+		fprintf(stderr, "Can't open file: no such file. (%s)\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	return;
 }
 
-// Функция трансляции программы
-void translate_program(char *input_file, char *output_file) {
-    FILE *input = fopen(input_file, "r");
-    FILE *output = fopen(output_file, "wb");
+void translation(const char* filename)
+{
+	for(int i = 0; !feof(input); i++)
+	{
+		char line[255];
+		if(!fgets(line, 255, input))
+		{
+			if(feof(input))
+			{
+				break;
+			}
+			else
+			{
+				fprintf(stderr, "Line %d: can't read line from file.\n", i++);
+				break;
+			}
+		}
+		char* addr;
+		char* command;
+		char* oper;
+		int num_of_cmd = 0;
+		int operand = 0;
+		int address = 0;
+	
+		char *ptr = strtok(line, " ");
+		addr = ptr;
+		
+		ptr = strtok(NULL, " ");
+		command = ptr;
+		
+		ptr = strtok(NULL, " +");
+		oper = ptr;
+		address = atoi(addr);
 
-    if (input == NULL || output == NULL) {
-        printf("Ошибка открытия файлов.\n");
-        return;
-    }
+		ptr = strtok(NULL," ");
 
-    int instructions[MEM_SIZE];
-    for (int i = 0; i < MEM_SIZE; i++)
-    {
-        instructions[i] = 0;
-    }
-    
-    char line[256];
+		if (ptr != NULL && ptr[0] != ';')
+		{
+			fprintf(stderr, "Line %d: unexpected symbols.\n", ++i);
+			break;
+		}
+		
+		if(!atoi(addr) && strcmp(addr, "00") != 0)
+		{
+			fprintf(stderr, "Line %d: expected address of memory cell.\n", ++i);
+			break;
+		}
+		//!!!
+		operand = atoi(oper);
+		char buffer[255];
+		sprintf(buffer,"%x",operand);
+		sscanf(buffer,"%x",&operand);
+		if((operand == NULL && (!strcmp(command, "HALT") || (strcmp(oper, "0\n") != 0 && strcmp(oper, "00\n") != 0 && strcmp(oper, "000\n") != 0 && strcmp(oper, "0000\n") != 0))))
+		{
+			if ((strcmp(command,"=") != 0))
+			{
+				fprintf(stderr, "Line %d: wrong operand.\n", ++i);
+				exit(EXIT_FAILURE);
+			}
+		}
 
-    // Считывание и трансляция инструкций
-    while (fgets(line, sizeof(line), input)) {
-        int address, operand, value;
-        char command[10];
-        
-        if (line[0] == '=') { // Если строка начинается с "=", то это задание значения ячейки памяти
-            sscanf(line, "= +%X ;", &operand);
-            // instructions[address].address = address;
-            // instructions[address].opcode = ASSIGNMENT;
-            // instructions[address].operand = operand;
-        } else {
-            sscanf(line, "%d %s %X", &address, command, &operand);
-            int opcode = get_opcode(command);
-            if (opcode == -1) {
-                printf("Неизвестная команда: %s\n", command);
-                return;
-            }
-            sc_commandEncode(0,opcode, operand, &value);
+		if(!strcmp(command, "READ"))
+		{
+			num_of_cmd = READ;
+		}
+		else if(!strcmp(command, "WRITE"))
+		{
+			num_of_cmd = WRITE;
+		}
+		else if(!strcmp(command, "LOAD"))
+		{
+			num_of_cmd = LOAD;
+		}
+		else if(!strcmp(command, "STORE"))
+		{
+			num_of_cmd = STORE;
+		}
+		else if(!strcmp(command, "ADD"))
+		{
+			num_of_cmd = ADD;
+		}
+		else if(!strcmp(command, "SUB"))
+		{
+			num_of_cmd = SUB;
+		}
+		else if(!strcmp(command, "DIVIDE"))
+		{
+			num_of_cmd = DIVIDE;
+		}
+		else if(!strcmp(command, "MUL"))
+		{
+			num_of_cmd = MUL;
+		}
+		else if(!strcmp(command, "JUMP"))
+		{
+			num_of_cmd = JUMP;
+		}
+		else if(!strcmp(command, "JNEG"))
+		{
+			num_of_cmd = JNEG;
+		}
+		else if(!strcmp(command, "JZ"))
+		{
+			num_of_cmd = JZ;
+		}
+		else if(!strcmp(command, "HALT"))
+		{
+			num_of_cmd = HALT;
+		}
+		else if(!strcmp(command, "AND"))
+		{
+			num_of_cmd = AND;
+		}
+		else if(!strcmp(command, "OR"))
+		{
+			num_of_cmd = OR;
+		}
+		else if(command[0] == '=')
+		{
+			sscanf(oper,"%x",&operand);
+			sc_memorySet(address, operand);
+			continue;
+		}
+		else
+		{
+			fprintf(stderr, "Line %d: wrong command.\n", ++i );
+			break;
+		}
 
-
-            instructions[address] = value;
-            // instructions[address].address = address;
-            // instructions[address].opcode = opcode;
-            // instructions[address].operand = operand;
-        }
-    }
-
-    // Запись инструкций в файл
-    fwrite(instructions, sizeof(int), MEM_SIZE, output);
-
-    fclose(input);
-    fclose(output);
+		int value = 0;
+		if(sc_commandEncode(0, num_of_cmd, operand, &value) < 0)
+		{
+			fprintf(stderr, "Line %d: error by encode. Check command or operand.\n", ++i );
+			break;
+		}
+		sc_memorySet(address, value);
+	}
+	//sc_regSet(1,1);
+	//sc_regSet(FLAG_IGNOR_PULS,1);
+	sc_memorySave(strdup(filename));
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Использование: %s файл.sa файл.o\n", argv[0]);
-        return 1;
-    }
-
-    translate_program(argv[1], argv[2]);
-
-    return 0;
+int main(int argc, const char** argv)
+{
+	if (argc < 3)
+	{
+		fprintf(stderr, "Usage: %s input.sa output.o\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	sc_memoryInit();
+	load_file(argv[1]);
+	translation(argv[2]);
+	return 0;
 }
